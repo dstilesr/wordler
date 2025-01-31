845e978e21ec96aee8f3217fd17370e759f21815
dstilesr <d.stiles.r@hotmail.com>
2025-01-30 19:24:13 -0500

    Add settings for the game environment

---
+++ wordler/environment/settings.py
@@ -0,0 +1,28 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class EnvSettings(BaseSettings):
+    """
+    Settings for the game environment.
+    """
+    invalid_word_penalty: float = Field(
+        -1.0,
+        description="Penalty for submitting an invalid word.",
+        lt=0.0,
+    )
+    win_reward: float = Field(
+        10.0,
+        description="Reward for winning a game.",
+        gt=0.0,
+    )
+    correct_letter_reward: float = Field(
+        1.0,
+        description="Reward for placing a letter correctly.",
+        gt=0.0,
+    )
+    letter_present_reward: float = Field(
+        0.5,
+        description="Reward for placing a correct letter in the wrong position.",
+        gt=0.0,
+    )
